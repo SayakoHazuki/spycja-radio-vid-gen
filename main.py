5d07c229ec36cc2d709a14ccf61d9b6a17fc1345
Kai Kamf <amf.kano@gmail.com>
2023-12-06 22:00:36 +0800

    Frame Complete Draft

--- main.py
+++ main.py
@@ -20,9 +20,7 @@ NOTO_SANS_FONT = "Noto-Sans-JP"
 SCREENSIZE = (1080,1920)
 
 def prioritizeImageSize(path, target_width, target_height):
-    pilImage = Image.open(path)
-    height = pilImage.height
-    width = pilImage.width
+    (width, height) = getSize(path)
     testFWidth = int(width * (target_height/height))
     if (testFWidth < target_width):
         priority = "w"
@@ -33,14 +31,17 @@ def prioritizeImageSize(path, target_width, target_height):
     fWidth = target_width if priority == "w" else None
     
     return (fWidth, fHeight)
-    
+
+def getSize(path):
+    pilImage = Image.open(path)
+    height = pilImage.height
+    width = pilImage.width
+    return (width, height) 
     
 
-# Getting proper size for covering background image
-print("Loading background image (PIL)")
-backgroundImageSize = prioritizeImageSize(BACKGROUND_IMAGE, SCREENSIZE[0], SCREENSIZE[1])
-
+# Background
 print("Creating backgroundClip")
+backgroundImageSize = prioritizeImageSize(BACKGROUND_IMAGE, SCREENSIZE[0], SCREENSIZE[1])
 backgroundClip = mpy.ImageClip(BACKGROUND_IMAGE, duration=10)
 backgroundClip = backgroundClip.set_position('center')
 backgroundClip = backgroundClip.resize(width=backgroundImageSize[0], height=backgroundImageSize[1]).set_duration(10)
@@ -48,24 +49,28 @@ backgroundClip = backgroundClip.resize(width=backgroundImageSize[0], height=back
 # Header
 print("Creating logoClip")
 logoClip = mpy.ImageClip(LOGO_IMAGE)
-logoClip = logoClip.set_position((60, 132))
+logoClip = logoClip.set_position((74, 132))
 logoClip = logoClip.resize(height=84)
 
 print("Creating dateClip")
 date = datetime.datetime.now().strftime("%d.%m.%Y")
 dateClip = mpy.TextClip(date, fontsize=64, color='white', font=MAKINAS_FONT)
-dateClip = dateClip.set_position((60, 228))
+dateClip = dateClip.set_position((74, 228))
 
 print("Creating tipClip")
 tip = "SOUND ON"
 tipClip = mpy.TextClip(tip, fontsize=40, color='white', font=MAKINAS_FONT)
-tipClip = tipClip.set_position((60, 292))
+tipClip = tipClip.set_position((74, 292))
 
+# Song info
 print("Creating thumbnailClip")
 thumbnailClipSize = prioritizeImageSize(THUMBNAIL_IMAGE, 142, 142)
+thumbnailSize = getSize(THUMBNAIL_IMAGE)
 thumbnailClip = mpy.ImageClip(THUMBNAIL_IMAGE)
 thumbnailClip = thumbnailClip.set_position((74,1702))
 thumbnailClip = thumbnailClip.resize(width=thumbnailClipSize[0], height=thumbnailClipSize[1])
+thumbnailClipW, thumbnailClipH = thumbnailClip.size
+thumbnailClip = thumbnailClip.crop(x_center=thumbnailClipW/2, y_center=thumbnailClipH/2, width=142, height=142)
 
 print("Creating titleClip")
 titleClip = mpy.TextClip(SONG_TITLE, fontsize=48, color='white', font=MAKINAS_FONT)
@@ -76,7 +81,7 @@ artistClip = mpy.TextClip(SONG_ARTIST, fontsize=36, color='white', font=MAKINAS_
 artistClip = artistClip.set_position((229, 1748))
 
 print("Creating postAuthorClip")
-postAuthorClip = mpy.TextClip(POST_AUTHOR, fontsize=36, color='white', font=NOTO_SANS_FONT)
+postAuthorClip = mpy.TextClip(f"投稿: {POST_AUTHOR}", fontsize=36, color='white', font=NOTO_SANS_FONT)
 postAuthorClip = postAuthorClip.set_position((229, 1792))
 
 
@@ -84,8 +89,9 @@ postAuthorClip = postAuthorClip.set_position((229, 1792))
 print("Compositing")
 final = mpy.CompositeVideoClip([backgroundClip, logoClip, dateClip, tipClip, thumbnailClip, titleClip, artistClip, postAuthorClip], size=SCREENSIZE)
 # final.subclip(0,5).write_videofile(f"./output/{datetime.datetime.now().timestamp()}.mp4", fps=24, codec="libx264")
-# final.show(1.5)
+
 print("Saving preview")
 final.save_frame("frame.png", t=1.5) 
+
 print("Exported preview. Opening...")
 os.startfile("frame.png")
